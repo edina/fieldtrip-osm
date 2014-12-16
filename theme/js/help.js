f97ac8b6a9563e097aeb85dd9aac427af5beba3f
Ruben Gamez <ruben.gamez@ed.ac.uk>
2014-12-16 16:59:38 +0000

    Open the help link in a new window
    Issue edina/fieldtrip-gb#151

--- theme/js/help.js
+++ theme/js/help.js
@@ -2,4 +2,12 @@
 
 require(['utils'], function(utils){
     $("#help-internals .version").html(utils.version);
+
+    $('a.external-link').on('vclick', function(evt){
+        evt.preventDefault();
+
+        window.open(evt.currentTarget.href,
+                    '_blank',
+                    'location=yes');
+    });
 });
